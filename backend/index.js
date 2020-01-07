e6079e82992cc33a9325278d8cdb6b67471126af
Jordan <iraborjordan1@gmail.com>
2020-01-07 05:23:07 +0100

    set up the backend to listen on port 9000

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const api = require('./routes/api')
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', api)
+
+mongoose.connect('mongodb://localhost:27017/pets');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection Error'))
+db.once('open', () => {
+    app.listen(9000, () => {
+        console.log('Running on port 9000')
+    })
+})
